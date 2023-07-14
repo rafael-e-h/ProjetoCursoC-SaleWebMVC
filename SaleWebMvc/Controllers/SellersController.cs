a1b003381ca9130a9d318bd43d370a7125ffa64d
rafael-e-h <rafael-e-h@hotmail.com>
2023-07-14 17:39:01 -0300

    Simple Create form

--- SaleWebMvc/Controllers/SellersController.cs
+++ SaleWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
@@ -18,6 +19,19 @@ namespace SalesWebMvc.Controllers
             var list = _sellerService.FindAll(); //Model acceess and return the data of List
 
             return View(list); //Show the View with this data
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]          //anotation to indicatte that this action is post, not get
+        [ValidateAntiForgeryToken] //prevent cross-site request forgeru (XSRF/CSRF) attacl in asp.net core
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
